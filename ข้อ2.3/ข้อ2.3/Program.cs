a82597b059cf1b680f6d6a737a6d5722aa7ab6de
ffrsr <rossarin.869@mail.kmutt.ac.th>
2021-05-29 21:24:43 +0700

    ข้อ2.3

---
+++ ข้อ2.3/ข้อ2.3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ข้อ2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money;
+
+            Console.Write("Please input money :");
+            money = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Vat : {0} bath", Math.Round(money * 0.07));
+            
+            Console.WriteLine("Total money : {0} bath", money + Math.Round(money * 0.07));
+            
+            Console.ReadLine();
+        }
+       
+    }
+}
